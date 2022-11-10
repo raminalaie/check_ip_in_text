e5e711bf79de4c6b040d981d4fc9e62d65520d02
raminalaie.79@gmail.com <109432088+raminalaie@users.noreply.github.com>
2022-11-10 12:25:04 +0330

    change list

--- ipcheck.py
+++ ipcheck.py
@@ -1,18 +1,18 @@
 import re
+
 data = [ "255.168.3.52 dsafds f",
       "1564653.355463.5463.46314",
       "dsa;fhd skfhla jfdiuh fudhcishf",
       "f sdljhfl 192.145.24.1",
-      "dsfnhisdfk",
+      "dsfnhisdfk 192.168.1.1",
       "dfjshhfshkd"]
 
 f = []
 for i in data:
       p =i.split(" ")
       f.append(p)
-# print(f)
-def check(Ip):
 
+def check(Ip):
 	regex = "^((25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\.){3}(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])$"
 	if(re.search(regex, Ip)):
 		return 1
@@ -20,14 +20,10 @@ def check(Ip):
 	else:
 		return 0	
 nah = [] 
-    
+   
 for i in f:
       for j in i:
-            nah.append(j)              
-pp=[]
-
-for j in nah:
-      if check(j) == 1:
-            pp.append(j)
+            if check(j) == 1:
+                  nah.append(j)
             
-print(pp)            
+print(nah)            
